8b0706a53636680841ed39d1d85090ef51e26087
Jacky Huang <lashhw81@gmail.com>
2023-06-28 15:22:03 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-for i in {100..200}; do
+for i in {100..500}; do
     echo "====="
     ratio=$(bc -l <<< "scale=2; ${i}/100")
     cmake-build-debug/bvh_incremental model.ply ray_100000.bin 2 "${ratio}"
